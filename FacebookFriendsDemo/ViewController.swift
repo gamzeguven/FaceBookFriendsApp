0818cf6494d156f36be48c717282784323a0ab70
gamzeguven <gamze.guven@bil.omu.edu.tr>
2020-03-08 17:25:14 +0300

    Initial Commit

---
+++ FacebookFriendsDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FacebookFriendsDemo
+//
+//  Created by Gamze Güven on 8.03.2020.
+//  Copyright © 2020 Gamze Güven. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
